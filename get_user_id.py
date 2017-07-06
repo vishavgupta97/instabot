a4308e42f650d096199e30f10eec81b312a30e6a
vishavgupta97 <vishavgupta988@gmail.com>
2017-07-06 13:17:02 +0530

    Defined the method get_user_id

---
+++ get_user_id.py
@@ -0,0 +1,14 @@
+from constants import BASE_URL,APP_ACCESS_TOKEN
+import requests
+def get_user_id(insta_username) :
+    request_url=BASE_URL+"users/search?q=%s&access_token="%(insta_username,APP_ACCESS_TOKEN)
+    print "GET REQUEST URL :%s"%(request_url)
+    user_info=requests.get(request_url).json()
+    if user_info['meta']['code']==200 :
+        if len(user_info['data']) :
+            return user_info['data'][0]['id']
+        else :
+            return None
+    else :
+        print "Status Code Other Than 200 Is Recieved\n"
+        exit()
