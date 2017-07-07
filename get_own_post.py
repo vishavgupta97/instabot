8967eb8d67bdd6ef3eade7317ae42526bca502e8
vishavgupta97 <vishavgupta988@gmail.com>
2017-07-07 08:45:12 +0530

    Added the feature of downloading image from instagram with the help of urllib library

--- get_own_post.py
+++ get_own_post.py
@@ -1,7 +1,20 @@
 from constants import  BASE_URL,APP_ACCESS_TOKEN
 import requests
+import urllib
 def get_own_post() :
     requests_url=(BASE_URL)+"users/self/media/recent/?access_token=%s"%(APP_ACCESS_TOKEN)
-    print "Get URL is:%s"%(requests_url)
+    print "Get Request URL:%s"%(requests_url)
     own_media=requets.get(requests_url).json()
+    if own_media['meta']['code']==200 :
+        if len(own_media['data']) :
+            image_name = own_media['data'][0]['id'] + '.jpeg'
+            image_url = own_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'Your image has been downloaded!'
+            #return own_media['data'][0]['id']
+        else :
+            print "!!!Post Doesn't exists!!!"
+    else :
+        print "STATUS CODE OTHER THAN 200 IS FOUND!"
+    #return None
 
---
+++ get_user_post.py
@@ -0,0 +1,26 @@
+from constants import BASE_URL,APP_ACCESS_TOKEN
+from get_user_id import get_user_id
+import urllib
+import requests
+def get_users_post(insta_username) :
+    user_id=get_user_id(insta_username)
+    if user_id==None :
+        print "Oooop!!!!!!User Doesn't Exists!!!!!"
+        exit()
+    request_url=BASE_URL+"users/%s/media/recent/?access_token=%s"%(user_id,APP_ACCESS_TOKEN)
+    print "Get Request URL:%s"%(request_url)
+    user_media=requests.get(request_url).json()
+    if user_media['meta']['data']==200 :
+        if len(user_media['data']) :
+            image_name = user_media['data'][0]['id'] + '.jpeg'
+            image_url = user_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print "Your image has been downloaded!"
+            #return user_media['data'][0][id]
+        else :
+            print "THERE IS NOT ANY RECENT POSTS"
+    else :
+        print "Status Code Other Than 200 Is Recieved"
+    #return None
+
+
